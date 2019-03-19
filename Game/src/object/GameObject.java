549718eba1df98a90c0dabfa6e4dae29765d087a
emmanuel ramirez <maneroto@hotmail.com>
2019-03-19 10:00:05 -0600

    Añadir GameObject

---
+++ Game/src/object/GameObject.java
@@ -0,0 +1,112 @@
+package object;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
+
+public abstract class GameObject {
+
+	protected double x;
+	protected double y;
+	protected int width;
+	protected int height;
+	protected BufferedImage imagen;
+	protected double velX;
+	protected double velY;
+
+	public GameObject(BufferedImage imagen)
+	{
+		this.imagen = imagen;
+	}
+
+	public GameObject(double x, double y)
+	{
+		this.x = x;
+		this.y = y;
+	}
+
+	public boolean placeMeeting(double x, double y, GameObject o)
+	{
+		if ((new Rectangle((int)x, (int)y , width, height)).intersects(o.getBounds())) 
+			return true;
+		return false;
+	}
+
+	public double clamp(double var, double min, double max)
+	{
+		if (var >= max) return var = max;
+		else if(var <= min) return var = min;
+		else return var;
+	}
+
+	public abstract void tick();
+
+	public abstract void render(Graphics g);
+
+	public abstract Rectangle getBounds();
+
+	public void setX(double x)
+	{
+		this.x = x;
+	}
+
+	public void setY(int y)
+	{
+		this.y = y;
+	}
+
+	public void setSize(int width, int height)
+	{
+		this.width = width;
+		this.height = height;
+	}
+
+	public void setImagen(BufferedImage imagen)
+	{
+		this.imagen = imagen;
+	}
+	
+	public void setVelX(int velX)
+	{
+		this.velX = velX;
+	}
+
+	public void setVelY(int velY)
+	{
+		this.velY = velY;
+	}
+
+	public int getX()
+	{
+		return (int)x;
+	}
+
+	public int getY()
+	{
+		return (int)y;
+	}
+
+	public int getWidth()
+	{
+		return width;
+	}
+
+	public int getHeight()
+	{
+		return height;
+	}
+
+	public BufferedImage getImagen()
+	{
+		return imagen;
+	}
+
+	public int getVelX()
+	{
+		return (int)velX;
+	}
+
+	public int getVelY()
+	{
+		return (int)velY;
+	}
+}
---
+++ Game/src/tile/Tile.java
@@ -0,0 +1,50 @@
+package tile;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.awt.Rectangle;
+import object.GameObject;
+
+public class Tile extends GameObject{
+
+	public static Tile[]tiles = new Tile[256];
+	
+	
+	protected int iden;
+	public static final int WIDTH = 64;
+	public static final int HEIGHT = 64;
+
+	public Tile(BufferedImage imagen, int iden) {
+		super(imagen);
+		this.iden = iden;
+		tiles[iden] = this;
+		width = WIDTH;
+		height = HEIGHT;
+	}
+	public void tick() {
+		
+		
+	}
+
+	public boolean isSolid()
+	{
+		return false;
+	}
+
+	public Rectangle getBounds()
+	{
+		return (new Rectangle((int)x, (int)y, width, height));
+	}
+
+	public void render(Graphics g)
+	{
+		g.drawImage(imagen, (int)x, (int)y, width, height, null);
+	}
+
+	public void render(Graphics g, int x, int y)
+	{
+		this.x = x;
+		this.y = y;
+		g.drawImage(imagen, x, y, width, height, null);
+	}
+}
