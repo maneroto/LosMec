d483442dff8bb8857c1489c10644d31dcca971f9
AlonsoOropeza <alonso.oropeza@live.com.mx>
2019-04-04 21:00:54 -0600

    Add beta para esta entrega

---
+++ The-Quest-Finder-Java-Game--master/TheQuestFinder/src/tiles/Tile.java
@@ -0,0 +1,8 @@
+package tiles;
+
+public class Tile {
+
+	public static final int WIDTH = 50;
+	public static final int HEIGHT = 50;
+
+}
---
+++ The-Quest-Finder-Java-Game--master/TheQuestFinder/src/worlds/World.java
@@ -0,0 +1,43 @@
+package worlds;
+
+import java.awt.Graphics;
+
+import caracteristicasPrincipales.GameObject;
+import controlDeObjetos.Handler;
+
+public class World {
+
+	public World(Handler handler, String string) {
+	}
+
+	public int getSpawnY() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getSpawnX() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public GameObject getTile(int x, int y) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
